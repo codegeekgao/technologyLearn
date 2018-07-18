8f1f67317b04584ffc0f2448a1d667a261daa763
codegeekgao <codegeek_mrgao@163.com>
2018-07-18 18:16:38 +0800

    mybatis 逆向工程生成mapper映射文件以及接口相关实体类。

---
+++ mybatis/src/main/java/com/donniegao/GeneratorSqlmap.java
@@ -0,0 +1,46 @@
+package com.donniegao;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorSqlmap {
+
+    public void generator() throws Exception {
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        String path = this.getClass().getClassLoader().getResource("generatorConfig.xml").getPath();
+        File configFile = new File(path);
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+
+    /**
+     * 使用mybatis逆向工程生成映射文件和接口
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
