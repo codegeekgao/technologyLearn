de6b27927b8311107238ad77243fc0e96603187a
codegeekgao <codegeek_mrgao@163.com>
2018-12-17 17:49:03 +0800

    :sparkles: sha1算法

--- jdk/src/main/java/com.codegeek/security/sha1/Sha1Encode.java
+++ jdk/src/main/java/com.codegeek/security/sha1/Sha1Encode.java
@@ -1,4 +1,4 @@
-package com.codegeek.security.md5;
+package com.codegeek.security.sha1;
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
@@ -8,11 +8,18 @@ import java.security.NoSuchAlgorithmException;
 /**
  * 摘要算法
  */
-public class Md5Encode {
+public class Sha1Encode {
 
 
     public static void main(String[] args) {
-
+        // hash若一致则称发生了碰撞
+        System.out.println("abc".hashCode() == "xyz".hashCode());
+        /**
+         * 摘要算法：MD5，SHA-1，SHA-256
+         */
+        /**
+         * 彩虹表MD5 不能直接MD5可以加盐，保证相对安全
+         */
         try {
             // 16进制数目
             System.out.println(String.format("%32x",new BigInteger(1,Md5Encode("haha123".getBytes("UTF-8")))));
@@ -24,7 +31,7 @@ public class Md5Encode {
     private static byte[] Md5Encode(byte[] input) {
         MessageDigest messageDigest = null;
         try {
-            messageDigest = MessageDigest.getInstance("sha1");
+            messageDigest = MessageDigest.getInstance("MD5");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
