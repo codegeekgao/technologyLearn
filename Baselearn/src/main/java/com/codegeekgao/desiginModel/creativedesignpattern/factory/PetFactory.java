b986665ebec271d4be0ed67b8902850d39e1abbf
codegeekgao <codegeek_mrgao@163.com>
2018-12-24 14:46:53 +0800

    :sparkles: 工厂方法模式,日志类实现。彩种实现

--- Baselearn/src/main/java/com/codegeekgao/desiginModel/creativedesignpattern/factory/PetFactory.java
+++ Baselearn/src/main/java/com/codegeekgao/desiginModel/creativedesignpattern/factory/PetFactory.java
@@ -19,15 +19,20 @@ public class PetFactory {
         } else {
             pet = new Cat();
         }
+        System.out.println(pet.getClass());
         return pet;
     }
 
-
+    /**
+     * 工厂方法模式:当需要创建一个类，而在编程时不能确定这个类的类型时(需要运行时候确定)----参考不同彩种拆票的案列
+     * 当一个类希望由其子类来指定所创建对象的具体类型，这里可以在日志记录功能需要用到工厂方法模式,比如文件日志，数据库记录，控制台日志记录
+     *
+     */
     public static void main(String[] args) {
         // 创建工厂方法
         PetFactory petFactory = new PetFactory();
-        Pet pet = petFactory.getPet("汪汪");
-        // 此时你并不确切知道创建的是那个对象工厂
+        Pet pet = petFactory.getPet("喵喵");
+        // 光看Pet你并不确切知道创建的是那个对象工厂
         System.out.println(pet.petSound());
     }
 }
