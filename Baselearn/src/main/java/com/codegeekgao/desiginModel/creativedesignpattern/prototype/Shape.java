1e6c1f9999cc1321e17b3e8a3340e2041f9dce23
codegeekgao <codegeek_mrgao@163.com>
2018-12-25 10:35:13 +0800

    :sparkles: 原型设计模式很少用，适用于场景：创建一个对象花销高，只需克隆一个即可

---
+++ Baselearn/src/main/java/com/codegeekgao/desiginModel/creativedesignpattern/prototype/Shape.java
@@ -0,0 +1,39 @@
+/**
+ * zlc.com Inc.
+ * Copyright (c) 2018-2018 All Rights Reserved.
+ */
+package com.codegeekgao.desiginModel.creativedesignpattern.prototype;
+
+/**
+ * @author codegeekgao
+ * @version Id: Shape.java, v 0.1 2018/12/25 0025 10:31 codegeekgao Exp $$
+ */
+public abstract class Shape implements Cloneable {
+
+    private String id;
+    protected String type;
+
+    abstract void draw();
+
+    public String getType(){
+        return type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
