905e8cc5d700e5af628949164f3524abe8bb8d8b
codegeekgao <codegeek_mrgao@163.com>
2018-12-25 17:16:09 +0800

    :fire: 类适配器与对象适配器一致底层都是调用原有对象的方法

---
+++ Baselearn/src/main/java/com/codegeekgao/desiginModel/template/Jczq.java
@@ -0,0 +1,26 @@
+/**
+ * zlc.com Inc.
+ * Copyright (c) 2018-2018 All Rights Reserved.
+ */
+package com.codegeekgao.desiginModel.template;
+
+/**
+ * @author codegeekgao
+ * @version Id: Jczq.java, v 0.1 2018/12/25 0025 17:01 codegeekgao Exp $$
+ */
+public class Jczq extends Game {
+    @Override
+    void initialize() {
+        System.out.println("jczq 开始投注");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("jczq 待开奖");
+    }
+
+    @Override
+    void endPlay() {
+        System.out.println("jczq 开奖");
+    }
+}
