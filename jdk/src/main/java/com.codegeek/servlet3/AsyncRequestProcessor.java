b8cf97f3f6839fbd9e80cee4db346372b0b16e33
codegeekgao <codegeek_mrgao@163.com>
2019-04-27 23:29:27 +0800

    :sparkles: asynContext.complete()

--- jdk/src/main/java/com.codegeek/servlet3/AsyncRequestProcessor.java
+++ jdk/src/main/java/com.codegeek/servlet3/AsyncRequestProcessor.java
@@ -1,6 +1,7 @@
 package com.codegeek.servlet3;
 
 import javax.servlet.AsyncContext;
+import java.io.IOException;
 
 /**
  * 异步业务工作线程
@@ -26,6 +27,14 @@ public class AsyncRequestProcessor implements Runnable {
     public void run() {
         System.out.println("是否异步:"+asyncContext.getRequest().isAsyncSupported());
         longProcessing(time);
+        try {
+            // TODO 这里伪代码，具体的业务线程方法
+            asyncContext.getResponse().getWriter().write("处理时间："+time);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 异步调用
+        asyncContext.complete();
     }
 
     private void longProcessing(int time) {
